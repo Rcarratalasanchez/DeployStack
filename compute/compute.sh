ae84131790ca76af5708202ff6e9d0df6ed5cc57
Rcarratalasanchez <robertocs695@hotmail.com>
2015-02-14 19:33:46 +0100

    added more changes

---
+++ compute/compute.sh
@@ -0,0 +1,7 @@
+#########################
+# Chapter * COMPUTE  #
+#########################
+
+# This script is for deploy automatically the software that compute need for 
+# create a openstack structure (deploy in Vagrant)
+
